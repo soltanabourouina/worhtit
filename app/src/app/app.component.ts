eee1b2b5da981e216c8c2f10343acf99ff0ce02e
soltana bourouina <bourouina.soltana@gmail.com>
2021-07-04 20:51:59 +0200

    app.comp.ht vidé

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAXMqFNsnh5wDyX2I9vjQdoqAMwYRZLOvY",
+      authDomain: "worthitfr.firebaseapp.com",
+      projectId: "worthitfr",
+      storageBucket: "worthitfr.appspot.com",
+      messagingSenderId: "977698775690",
+      appId: "1:977698775690:web:2d715fe7186a6b16fec373"
+    };
+
+
+
+}
 }
